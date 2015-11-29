1a005f5ce8e76373e3878a6e866193f4ed51a2b4
Tanguy <pogonas91@hotmail.fr>
2015-11-29 21:42:34 +0100

    rm kernel line

--- src/BLOG/TemplateBundle/Controller/PageController.php
+++ src/BLOG/TemplateBundle/Controller/PageController.php
@@ -107,7 +107,7 @@ class PageController extends Controller{
 		$base = new Base();
 
 		// On crée le FormBuilder grâce au service form factory
-		$formBuilder = $this->get('form.factory')->createBuilder('form', $base);
+		$formBuilder = $this->get('Theme:form.factory')->createBuilder('form', $base);
 
 		// On ajoute les champs de l'entité que l'on veut à notre formulaire
 		$formBuilder
@@ -125,7 +125,7 @@ class PageController extends Controller{
 
 		// On passe la méthode createView() du formulaire à la vue
 		// afin qu'elle puisse afficher le formulaire toute seule
-		return $this->render('OCPlatformBundle:Advert:add.html.twig', array('form' => $form->createView(),));
+		return $this->render('BLOGTemplateBundle:Page:add.html.twig', array('form' => $form->createView(),));
 
 	  }
 		
