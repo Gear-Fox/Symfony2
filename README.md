c783637d522848085ba8896a42d7852d9a3575ca
Tanguy <pogonas91@hotmail.fr>
2015-11-29 16:40:55 +0100

    initial import

---
+++ README.md
@@ -0,0 +1,4 @@
+site
+====
+
+A Symfony project created on November 17, 2015, 9:59 am.
---
+++ src/BLOG/UserBundle/BLOGUserBundle.php
@@ -0,0 +1,9 @@
+<?php
+
+namespace BLOG\UserBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BLOGUserBundle extends Bundle
+{
+}
